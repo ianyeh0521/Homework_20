0be25e4cc8561d5ba1da43468a58c7aee168f180
Ian Yeh <ian052199@gmail.com>
2023-09-04 16:55:50 +0800

    correct some mistakes in Hw1 and Hw2

--- src/hw1/hw1.java
+++ src/hw1/hw1.java
@@ -3,26 +3,31 @@ package hw1;
 import java.security.DomainCombiner;
 import java.util.Iterator;
 
-public class hw1 {
+public class Hw1 {
 	
 	public static void main(String[] args) {
 		// 1.請設計一支 Java 程式，計算 12, 6 這兩個數值的和與積
 		System.out.println("12*6="+12*6+"，"+"12+6="+(12+6));
 		
+		
 		// 2.請設計一支 Java 程式，計算 200 顆蛋共是幾打幾顆?
 		System.out.printf("200 顆蛋共是%d打%d顆%n", 200/12, 200%12);
+		
 		
 		// 3.請由程式算出 256559 秒為多少天、多少小時、多少分與多少秒
 		System.out.printf("256559秒為%d天%d小時%d分與%d秒%n", 256559/86400, (256559%86400)/3600, ((256559%86400)%3600)/60, ((256559%86400)%3600)%60);
 	
+		
 		// 4.請定義一個常數為 3.1415(圓周率),並計算半徑為 5 的圓面積與圓周長
 		final double pi = 3.1415;
 		System.out.printf("半徑為 5 的圓面積為%f，圓周長為%f%n", 5*5*pi, 5*2*pi);
+		
 		
 		// 5.某人在銀行存入 150 萬,銀行利率為 2%,如果每年利息都繼續存入銀行,請用程式計算 10 年後,本金加利息共有多少錢
 		int m = 150;
 		double r = 0.02;
 		System.out.println("10年後，本金加利息共有"+(m*Math.pow((1+r), 10))+"萬元");
+		
 		
 		/* 6.請寫一隻程式,利用System.out.println()印出以下三個運算式結果:
 				5 + 5
--- src/hw2/hw2.java
+++ src/hw2/hw2.java
@@ -1,6 +1,6 @@
 package hw2;
 
-public class hw2 {
+public class Hw2 {
 
 	public static void main(String[] args) {
 
@@ -14,6 +14,7 @@ public class hw2 {
 		System.out.println("1. " + sum1);
 		System.out.println();
 
+		
 		// 2.請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用for迴圈)
 		int sum2 = 1;
 		for (int i = 1; i <= 10; i++) {
@@ -22,6 +23,7 @@ public class hw2 {
 		System.out.println("2. " + sum2);
 		System.out.println();
 
+		
 		// 3.請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用while迴圈)
 		int sum3 = 1;
 		int count1 = 1;
@@ -32,6 +34,7 @@ public class hw2 {
 		System.out.println("3. " + sum3);
 		System.out.println();
 
+		
 		// 4.請設計一隻Java程式,輸出結果為以下: 1 4 9 16 25 36 49 64 81 100
 		System.out.print("4. ");
 		for (int i = 1; i <= 10; i++) {
@@ -40,6 +43,7 @@ public class hw2 {
 		System.out.println();
 		System.out.println();
 
+		
 		// 5.阿文很熱衷大樂透 (1 ~ 49),但他不喜歡有4的數字,不論是個位數或是十位數。請設計一隻程式,輸出結果為阿文可以選擇的數字有哪些?總共有幾個?
 		System.out.print("5. ");
 		int countNum = 0;
@@ -54,6 +58,7 @@ public class hw2 {
 		System.out.println("總共有" + countNum + "個");
 		System.out.println();
 
+		
 		// 6.
 		System.out.println("6. ");
 		int count2 = 10;
@@ -66,6 +71,7 @@ public class hw2 {
 		}
 		System.out.println();
 
+		
 		// 7.
 		System.out.println("7. ");
 		int asciiValue = 65;
@@ -78,6 +84,7 @@ public class hw2 {
 		}
 		System.out.println();
 
+		
 		// 8. 99乘法表的組合（課堂練習）
 		// 1.使用 for 迴圈 + while 迴圈
 		System.out.println("8. ");
@@ -92,6 +99,7 @@ public class hw2 {
 		}
 		System.out.println();
 
+		
 		// 2.使用 for 迴圈 + do while 迴圈
 		System.out.println("使用 for 迴圈 + do while 迴圈：");
 		for (int i = 1; i <= 9; i++) {
@@ -104,6 +112,7 @@ public class hw2 {
 		}
 		System.out.println();
 
+		
 		// 3.使用 while 迴圈 + do while 迴圈
 		System.out.println("使用 while 迴圈 + do while 迴圈：");
 		int k = 1;
